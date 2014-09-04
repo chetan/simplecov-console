7b5678aa08cd8a5192cdf16dda26ab89a0f7a560
Francois de Villiers <villierf@amazon.com>
2014-09-04 11:20:17 +0200

    Updated MiniTest for Ruby 2.0, added test for missed method.

--- test/test_simplecov-console.rb
+++ test/test_simplecov-console.rb
@@ -1,8 +1,22 @@
 require 'helper'
 
-class TestSimplecovConsole < MiniTest::Unit::TestCase
+class TestSimplecovConsole < MiniTest::Test
+
+  Source = Struct.new(:line_number)
+
+  def setup
+    @console = SimpleCov::Formatter::Console.new
+  end
+
   def test_defined
     assert defined?(SimpleCov::Formatter::Console)
     assert defined?(SimpleCov::Formatter::Console::VERSION)
+  end
+
+  def test_missed
+    missed_lines = [Source.new(1), Source.new(2),
+                    Source.new(3), Source.new(5)]
+    expected_result = ["1-3", "5"]
+    assert_equal @console.missed(missed_lines), expected_result
   end
 end
