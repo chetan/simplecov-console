55ab7b50dcbf2c533e72256365c5e5ec6f034c98
Dana Sherson <robot@dana.sh>
2020-11-03 17:25:18 +1100

    Add branch coverage output
    This will only show if simlpecov is recording branch coverage, otherwise
    the output is unchanged
    
    The output does look kinda awkward and busy, but all i care about is
    actually being able to see output from CI without having to set up some
    kind of artifact hosting for the coverage/index.html
    
    fixes: #18
    
    example output:
    
    Passing:
    ```
    COVERAGE: 100.00% -- 1743/1743 lines in 108 files
    BRANCH COVERAGE: 100.00% -- 434/434 branches in 108 branches
    ```
    
    Failing (block format):
    ```
    COVERAGE: 100.00% -- 1743/1743 lines in 108 files
    BRANCH COVERAGE:  99.77% -- 434/435 branches in 108 branches
    
        file: lib/leftovers/config_validator/error_processor.rb
    coverage: 100.00% (96/96 lines)
      missed:
    branches: 96.97% (32/33 branches)
      missed: 112[else]
    ```
    
    Failing (table format):
    ```
    COVERAGE: 100.00% -- 1743/1743 lines in 108 files
    BRANCH COVERAGE:  99.77% -- 434/435 branches in 108 branches
    
    +----------+---------------------------------------------------+-------+--------+---------+-----------------+----------+-----------------+------------------+
    | coverage | file                                              | lines | missed | missing | branch coverage | branches | branches missed | branches missing |
    +----------+---------------------------------------------------+-------+--------+---------+-----------------+----------+-----------------+------------------+
    | 100.00%  | lib/leftovers/config_validator/error_processor.rb | 96    | 0      |         |  96.97%         | 33       | 1               | 112[else]        |
    +----------+---------------------------------------------------+-------+--------+---------+-----------------+----------+-----------------+------------------+
    ```

--- lib/simplecov-console/output/block.rb
+++ lib/simplecov-console/output/block.rb
@@ -3,7 +3,7 @@ class SimpleCov::Formatter::Console
   module BlockOutput
 
     # format per-file results output as plain text blocks
-    def output(files, root)
+    def output(files, root, show_branch)
       blocks = []
       files.each do |f|
         block = []
@@ -12,6 +12,12 @@ class SimpleCov::Formatter::Console
                     colorize(sprintf("%.2f%%", f.covered_percent)),
                     f.covered_lines.count, f.lines_of_code)
         block << sprintf("%8.8s: %s", 'missed', missed(f.missed_lines).join(", "))
+        if show_branch
+          block << sprintf("%8.8s: %s (%d/%d branches)", 'branches',
+                    colorize(sprintf("%.2f%%", f.coverage_statistics[:branch].percent)),
+                    (f.total_branches.count - f.missed_branches.count), f.total_branches.count)
+          block << sprintf("%8.8s: %s", 'missed', branches_missed(f.missed_branches).join(", "))
+        end
         blocks << block.join("\n")
       end
 
--- lib/simplecov-console/output/table.rb
+++ lib/simplecov-console/output/table.rb
@@ -4,15 +4,25 @@ class SimpleCov::Formatter::Console
   module TableOutput
 
     # format per-file results output using Terminal::Table
-    def output(files, root)
+    def output(files, root,show_branch)
       table = files.map do |f|
-        [
-          colorize(pct(f)),
+        row = [
+          colorize(pct(f.covered_percent)),
           f.filename.gsub(root + "/", ''),
           f.lines_of_code,
           f.missed_lines.count,
-          missed(f.missed_lines).join(", ")
+          missed(f.missed_lines).join(", "),
         ]
+        if show_branch
+          row += [
+            colorize(pct(f.coverage_statistics[:branch].percent)),
+            f.total_branches.count,
+            f.missed_branches.count,
+            branches_missed(f.missed_branches).join(", ")
+          ]
+        end
+
+        row
       end
 
       table_options = SimpleCov::Formatter::Console.table_options || {}
@@ -21,6 +31,14 @@ class SimpleCov::Formatter::Console
       end
 
       headings = %w{ coverage file lines missed missing }
+      if show_branch
+        headings += [
+          'branch coverage',
+          'branches',
+          'branches missed',
+          'branches missing'
+        ]
+      end
 
       opts = table_options.merge({:headings => headings, :rows => table})
       Terminal::Table.new(opts)
